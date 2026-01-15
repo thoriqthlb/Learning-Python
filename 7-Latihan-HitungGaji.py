f29abc0729dc1832eb513badbc762c41c22f4eb7
thoriqthlb <thalibthoriq4@students.unnes.ac.id>
2026-01-15 13:01:17 +0700

    latihan lanjutan

---
+++ 7-Latihan-HitungGaji.py
@@ -0,0 +1,23 @@
+print("\n===SLIP GAJI MAGANG===\n")
+
+nama = input("Masukkan nama: ")
+hari = int(input("Berapa hari Anda masuk: "))
+
+print("------------------------")
+
+saku = int(input("Saku harian: Rp"))
+gaji = int(input("Gaji harian: Rp"))
+
+print("------------------------")
+
+total = (saku + gaji) * hari
+
+# MENAMBAHKAN TITIK DI TIAP RIBUAN (PENYEMPURNAAN)
+print(f"{nama} secara total menerima: Rp{total:,}" .replace(",", "."), "dalam", hari, "hari.")
+
+### KODE AWAL YANG DIBUAT UNTUK ANGKA (TANPA TITIK)
+# print(nama, "secara total menerima: Rp", total, "dalam", hari, "hari.")
+
+
+### ILMU BARU UNTUK (INT) AGAR BISA ADA TITIKNYA
+# print(f"{nama} secara total menerima: Rp{total:,}".replace(',', '.') + " dalam", hari, "hari.")
