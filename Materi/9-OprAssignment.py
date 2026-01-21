ff0603798a0de312fb5ca6eba5534a9b2b237b7c
thoriqthlb <thalibthoriq4@students.unnes.ac.id>
2026-01-21 13:56:26 +0700

    operator assignment

---
+++ Materi/9-OprAssignment.py
@@ -0,0 +1,34 @@
+# operasi yang dapat dilakukan dengan penyingkatan
+
+a = 5 # ini assignment
+print("Nilai a =",a)
+
+a += 1 # artinya nilai a = a + 1
+print("Nilai a += 1 =",a)
+
+a -= 2 # artinya nilai a = a - 2
+print("Nilai a -= 2 =",a)
+
+a *= 5 # artinya nilai a = a * 5
+print("Nilai a *= 5 =",a)
+
+a /= 2 # artinya nilai a = a / 2
+print("Nilai a /= 2 =",a)
+
+b = 10
+print("\nNilai b = ", b)
+
+b %= 3
+print("Nilai b %= 3 =",b)
+
+b = 10
+print("\nNilai b = ", b)
+
+b //= 3
+print("Nilai b //= 3 =",b)
+
+b = 10
+print("\nNilai b = ", b)
+
+b **= 3
+print("Nilai b **= 3 =",b)
