7a51f5c4e65f8c4442f47b049e95ffa4b93bd5a4
thoriqthlb <thalibthoriq4@students.unnes.ac.id>
2026-01-27 18:10:09 +0700

    While Loop

---
+++ Materi/17-WhileLoop.py
@@ -0,0 +1,15 @@
+# while kondisi:
+#   aksi ini
+#   aksi itu
+
+# Akhir dari program
+
+angka = 0
+print(f"Angka sekarang => {angka} ")
+
+while angka <5:
+    angka += 1
+    print(f"Angka sekarang => {angka} ")
+    print("Oke Udin")
+
+print("Selesai.")
