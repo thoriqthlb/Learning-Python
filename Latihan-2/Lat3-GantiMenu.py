74e64fe92bea620f2532e501d1710aa0cfc582fb
thoriqthlb <thalibthoriq4@students.unnes.ac.id>
2026-02-05 15:50:42 +0700

    optimizing code

--- Latihan-2/Lat3-GantiMenu.py
+++ Latihan-2/Lat3-GantiMenu.py
@@ -4,8 +4,14 @@ print("=== DAFTAR MAKANAN ===")
 for index,makanan in enumerate(list_makanan):
     print(f"No. {index+1} | {makanan}")
 
-ganti = int(input("Mau ganti nomor berapa? (1-5): "))
-makanan_2 = input("Ganti apa?: ").title()
+while True:
+    jumlah_menu = len(list_makanan) # kalo diedit ga perlu nambah 1-1
+    ganti = int(input(f"Mau ganti nomor berapa?: (1-{jumlah_menu})"))
+    if ganti < 1 or ganti > jumlah_menu:
+        print("ERROR. PILIH ANGKA YANG SESUAI!")
+        continue # biar balik lagi
+    makanan_2 = input("Ganti apa?: ").title()
 
-list_makanan[ganti-1] = makanan_2
-print(list_makanan)
+    list_makanan[ganti-1] = makanan_2
+    print(list_makanan)
+    break
