0d41da3a5c00d9cf81aaa3317016bf1c80786047
thoriqthlb <thalibthoriq4@students.unnes.ac.id>
2026-02-09 14:47:17 +0700

    materi tuples dan sets

---
+++ Materi-2/8-TuplesDanSet.py
@@ -0,0 +1,14 @@
+# list, menggunakan kurung siku
+data_list = [10,2,3,4]
+print(data_list)
+
+# tuples, kumpulan data tapi gabisa diubah isinya
+# gabisa diubah, append, dll.
+data_tuples = (2,3,5,6,8,9,1)
+print(data_tuples)
+print(data_tuples[0])
+
+# sets (himpunan), gaada indexnya
+data_sets = {2,3,4,6,8}
+print(data_sets)
+# print(data_sets[0]) (GABISA)
