f32d2395c38d636cbe72dd926688bb3aedd72a41
thoriqthlb <thalibthoriq4@students.unnes.ac.id>
2026-02-09 15:01:29 +0700

    dictionary

---
+++ Materi-2/9-Dictionary.py
@@ -0,0 +1,17 @@
+# list => array, mengakses menggunakan index
+
+data_list = ["Hilmy","Rizki","Yahya"]
+print(data_list[2])
+
+# dictionary (dict) => associative array
+# identifier => key
+
+data_dict = {
+    "key" : "value",
+    "my" : "Hilmy",
+    "rz" : "Rizki",
+    "ya" : "Yahya",
+    "jt" : "Jhet"
+}
+
+print(data_dict["jt"])
