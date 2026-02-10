505c90492574e7bd712479bb513b9793b9de65af
thoriqthlb <thalibthoriq4@students.unnes.ac.id>
2026-02-10 17:26:26 +0700

    upgrade 2

--- Latihan-2/Lat9-AntreanTiket.py
+++ Latihan-2/Lat9-AntreanTiket.py
@@ -9,29 +9,48 @@ antrean_tiket = [
      "kendala": "Layar Blank"}
 ]
 
-# Tambahan dari input user 
-print("\n=== INPUT TIKET BARU ===")
-nama = input("Masukkan nama: ").title()
-kendala = input("Sebutkan kendalanya: ").title()
+while True:
+    print(f"""\n=== MENU IT SUPPORT ===
+1. Tambah Tiket Baru
+2. Lihat Semua Tiket
+3. Keluar                   
+""")
+    
+    pilihan = (input("Pilih menu (1-3): "))
 
-# Membuat urutan nomor selanjutnya dari nomor paling terakhir
-nomor_terakhir = antrean_tiket[-1]["no"]
-nomor_baru = nomor_terakhir + 1
+    if pilihan == "1":
+        # Tambahan dari input user 
+        print("\n=== INPUT TIKET BARU ===")
+        nama = input("Masukkan nama: ").title()
+        kendala = input("Sebutkan kendalanya: ").title()
 
-# Buat dictionary baru untuk dimasukin ke list (sebagai inputan user)
-tiket_baru = {
-    "no": nomor_baru, 
-    "user": nama, 
-    "kendala": kendala
-}
+        # Membuat urutan nomor selanjutnya dari nomor paling terakhir
+        nomor_terakhir = antrean_tiket[-1]["no"]
+        nomor_baru = nomor_terakhir + 1
 
-# Tambah dictionary yang baru dibuat ke list
-antrean_tiket.append(tiket_baru)
+        # Buat dictionary baru untuk dimasukin ke list (sebagai inputan user)
+        tiket_baru = {
+            "no": nomor_baru, 
+            "user": nama, 
+            "kendala": kendala
+        }
 
-# Cetak hasil akhir
-print("\n=== DAFTAR ANTREAN SAAT INI ===")
-for tiket in antrean_tiket:
-    print(f"No: {tiket.get('no')} \t| Nama: {tiket.get('user')} \t| Kendala: {tiket.get('kendala')}")
+        # Tambah dictionary yang baru dibuat ke list
+        antrean_tiket.append(tiket_baru)
+        print("Tiket berhasil didaftarkan!")
+
+    elif pilihan == "2":
+        # Cetak hasil akhir
+        print("\n=== DAFTAR ANTREAN SAAT INI ===")
+        for tiket in antrean_tiket:
+            print(f"No: {tiket.get('no')} \t| Nama: {tiket.get('user')} \t| Kendala: {tiket.get('kendala')}")
+
+    elif pilihan == "3":
+        print("Sistem dimatikan. Bye!")
+        break
+
+    else: 
+        print("ERROR. INPUT INVALID!!!")
 
 
 
