1e4b83ef48b06349047f07acdaecd0af7e92faef
thoriqthlb <thalibthoriq4@students.unnes.ac.id>
2026-02-10 16:42:12 +0700

    upgrade 1

--- Latihan-2/Lat9-AntreanTiket.py
+++ Latihan-2/Lat9-AntreanTiket.py
@@ -1,3 +1,4 @@
+# List berisi dictionary (detail informasi)
 antrean_tiket = [
     {"no": 101, 
     "user": "Andi", 
@@ -8,18 +9,34 @@ antrean_tiket = [
      "kendala": "Layar Blank"}
 ]
 
+# Tambahan dari input user 
 print("\n=== INPUT TIKET BARU ===")
 nama = input("Masukkan nama: ").title()
 kendala = input("Sebutkan kendalanya: ").title()
 
+# Membuat urutan nomor selanjutnya dari nomor paling terakhir
+nomor_terakhir = antrean_tiket[-1]["no"]
+nomor_baru = nomor_terakhir + 1
+
+# Buat dictionary baru untuk dimasukin ke list (sebagai inputan user)
 tiket_baru = {
-    "no": 103, 
+    "no": nomor_baru, 
     "user": nama, 
     "kendala": kendala
 }
 
+# Tambah dictionary yang baru dibuat ke list
 antrean_tiket.append(tiket_baru)
 
+# Cetak hasil akhir
 print("\n=== DAFTAR ANTREAN SAAT INI ===")
 for tiket in antrean_tiket:
     print(f"No: {tiket.get('no')} \t| Nama: {tiket.get('user')} \t| Kendala: {tiket.get('kendala')}")
+
+
+
+##### CATATAN #####
+
+# antrean_tiket => Adalah List (Gunakan .append(), .pop(), atau index []).
+# antrean_tiket[-1] => Adalah Dictionary (Gunakan .get() atau ["key"]).
+# antrean_tiket[-1]["no"] => Adalah Isi/Value dari dictionary tersebut (dalam hal ini, Angka).
