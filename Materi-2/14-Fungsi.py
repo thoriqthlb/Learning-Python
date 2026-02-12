84cf49445cee36102b1583dfe3bd3c234f2e614e
thoriqthlb <thalibthoriq4@students.unnes.ac.id>
2026-02-12 16:42:04 +0700

    materi fungsi

---
+++ Materi-2/14-Fungsi.py
@@ -0,0 +1,16 @@
+""" Membuat Fungsi """
+
+def fungsi():
+    '''pemanggilan fungsi harus dilakukan setelah definisi''' #py bahasa interpreted (kode dieksekusi dari atas)
+    print("Ini adalah fungsi.")
+
+# Panggil fungsinya
+fungsi()
+
+def hello_world():
+    '''fungsi menampilkan hello world'''
+    print("\nHello world!")
+    print("Apa kabar #####")
+    print("Have a good day!")
+
+hello_world()
