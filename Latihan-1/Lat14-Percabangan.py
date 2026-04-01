b89fd1361b1c7674f5cfaf9f8a2262000d84a505
thoriqthlb <thalibthoriq4@students.unnes.ac.id>
2026-04-01 17:54:13 +0700

    tambah latihan dasar

--- Latihan-1/Lat14-Percabangan.py
+++ Latihan-1/Lat14-Percabangan.py
@@ -24,3 +24,23 @@ elif operator == "/":
     print(f"Hasilnya adalah: {hasil}")
 else: print("Masukkan operator yang sesuai!")
 print("\nSelesai.")
+
+
+# Nyari nilai tertinggi
+
+nilai_siswa = [60, 85, 70, 90, 50]
+
+# 1. Anggap nilai pertama adalah yang tertinggi sementara
+juara = nilai_siswa[0] 
+
+# 2. Cek satu per satu
+for nilai in nilai_siswa:
+    
+    # 3. Jika ketemu nilai yang LEBIH BESAR dari juara saat ini...
+    # if nilai == max(nilai_siswa): (cara cepet)
+    if nilai > juara: # sesuai algoritma
+
+        # 4. Ganti posisi juara dengan nilai tersebut
+        juara = nilai
+
+print(f"\nNilai tertinggi adalah: {juara}\n")
