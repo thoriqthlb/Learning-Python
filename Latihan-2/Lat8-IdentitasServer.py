2753d6669c1107c0c78c49345ca5fda176804f23
thoriqthlb <thalibthoriq4@students.unnes.ac.id>
2026-04-07 14:31:58 +0700

    latihan tuple dan set

--- Latihan-2/Lat8-IdentitasServer.py
+++ Latihan-2/Lat8-IdentitasServer.py
@@ -18,3 +18,29 @@ server_it["lokasi"] = "Lantai 3"
 # Profil diperbarui
 for k,v in server_it.items():
     print(f"kode = {k} \t| isi = {v}")
+
+
+# Latihan Tuple
+koordinat_kumpul = (-6.200000, 106.816666)  # (Lintang, Bujur)
+
+# 1. Ambil garis Lintang (angka pertama) dari Tuple di atas
+lintang = koordinat_kumpul[0]
+
+# 2. Ambil garis Bujur (angka kedua)
+bujur = koordinat_kumpul[1]
+
+print("Titik kumpul berada di garis lintang:", lintang)
+
+
+# Latihan Set
+# Tumpukan data mentah dari survei (List)
+jawaban_survei = ["Membaca", "Berenang", "Membaca", "Lari", "Berenang", "Membaca"]
+
+# 1. Sulap list 'jawaban_survei' di atas menjadi Set agar duplikatnya terbuang!
+hobi_unik = set(jawaban_survei)
+
+# 2. Hitung ada BERAPA BANYAK macam hobi di dalam 'hobi_unik' sekarang
+jumlah_hobi = len(hobi_unik)
+
+print("Hobi unik yang ditemukan:", hobi_unik)
+print("Total macam hobi:", jumlah_hobi)
