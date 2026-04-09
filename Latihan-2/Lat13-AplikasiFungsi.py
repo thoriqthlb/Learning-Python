c94c31336460c705d55372ff3fdae0b611ff5ccb
thoriqthlb <thalibthoriq4@students.unnes.ac.id>
2026-04-09 16:11:32 +0700

    Latihan hitung ongkir

--- Latihan-2/Lat13-AplikasiFungsi.py
+++ Latihan-2/Lat13-AplikasiFungsi.py
@@ -1,5 +1,4 @@
 import os
-
 os.system("cls")
 
 # 1. Kalkulator Tagihan Listrik Sederhana
@@ -14,5 +13,26 @@ print(5*"-", "Kalkulator Listrik", 5*"-")
 input_user = int(input("Masukkan jumlah penggunaan listrik (kWh): "))
 total_penggunaan = hitung_bayar(input_user)
 
-print(f"Total yang harus dibayarkan: Rp{total_penggunaan:,}".replace(",", "."))
+print(f"Total yang harus dibayarkan: Rp{total_penggunaan:,}".replace(",","."))
+print(30*"-")
+
+
+# 2. Kalkulator Ongkos Kirim
+def hitung_ongkir(jarak, layanan):
+    tarif = jarak * 2500
+    
+    if layanan.lower() == "express":
+        tarif += 15000
+    else: 0
+
+    return tarif
+
+print(5*'-', "Express Delivery", 5*'-')
+
+jarak_pengiriman = int(input("\nBerapa jarak tempuh pengiriman? (km): "))
+jenis_layanan = input("Pilih jenis layanan Express/Reguler: ")
+
+total_biaya = hitung_ongkir(jarak_pengiriman, jenis_layanan)
+
+print(f"\n> Total biaya ongkir Anda adalah: Rp{total_biaya:,}".replace(",","."))
 print(30*"-")
